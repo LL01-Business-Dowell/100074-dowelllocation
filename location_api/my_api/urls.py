6cb62aeec8d64f244358d5d27741bf42c5680590
Eric Mbuthia <ericmbuthia11@gmail.com>
2022-08-06 07:13:06 +0300

    addition of new style and page

--- location_api/my_api/urls.py
+++ location_api/my_api/urls.py
@@ -4,13 +4,16 @@ from my_api import views
 
 urlpatterns = [
         path('', views.home),
+        path('home/', views.api, name='home'),
+
+        path('api/',views.api, name='api'),
 
     path('continents/<slug:username>/<slug:sessionId>/<slug:projectCode>/', views.ContinentList.as_view()),
     path('countries/<slug:username>/<slug:sessionId>/<slug:projectCode>/', views.CountryList.as_view()),
     # path('country/<int:pk>/', views.CountryDetail.as_view()),
     path('country/<slug:query_type>/<slug:query_value>/<slug:username>/<slug:sessionId>/<slug:projectCode>/', views.CountryDetail.as_view()),
     path('region/<int:pk>/', views.RegionDetail.as_view()),
-    path('region/<slug:query_type>/<slug:query_value>/<slug:username>/<slug:sessionId>/<slug:projectCode>/', views.RegionDetail.as_view()),
+    path('region/<slug:query_type>/<str:query_value>/<slug:username>/<slug:sessionId>/<slug:projectCode>/', views.RegionDetail.as_view()),
     path('target-function/', views.target_func, name='target-funtion'),
     path('target-func/<slug:collection>/<slug:fields>/<slug:mongoId>/<str:name>/', views.TargetFuntion.as_view(), name='target-func'),
     path('log-in-func/', views.log_in_func, name='log-in'),
@@ -21,6 +24,7 @@ urlpatterns = [
     path('local-regions/<int:pk>', views.local_regions, name='local-regions'),
     path('sync-func/', views.sync_func, name='sync-func'),
     path('rec-tz/', views.rec_tz, name='rec-tz'),
+
     
     path('display-req-resp/', views.DisplayReqResp.as_view(), name='display-req-resp'),
 
--- location_api/my_api/views.py
+++ location_api/my_api/views.py
@@ -1,6 +1,6 @@
 import collections
 from itertools import combinations_with_replacement
-from msilib.schema import Error
+# from msilib.schema import Error
 from time import monotonic, time
 from django.shortcuts import render, redirect
 from my_api.models import Countries, Regions, SubRegions, Continent, RequestsRec, TimeZoneDb
@@ -24,6 +24,21 @@ import requests
 import pprint
 # Create your views here.
 BASE_DIR = Path(__file__).resolve().parent.parent
+def home(request):
+    # session = request.GET.get("session_id", None)
+    # if session:
+    #     user = get_user_profile(session)
+    #     if user is None:
+    #         return redirect("https://100014.pythonanywhere.com/")
+    #     else:
+    #         print(user)
+    #         response_data = {'error': True, 'status': "not ok","user":user}
+    #         return JsonResponse(response_data)
+    # else:
+    #     return redirect("https://100014.pythonanywhere.com/")
+    return render(request, 'api/home.html')
+def api(request):
+    return render(request, 'api/api.html')
 def rec_tz(request):
     wanted_tz = pd.read_csv(BASE_DIR / 'utcDf.csv')
     column_d = ["UTC", "time_zone","country","city"]
@@ -41,21 +56,7 @@ def rec_tz(request):
 
     response_data = {'error': True, 'status': "not ok"}
     return JsonResponse(response_data)
-def home(request):
-    session = request.GET.get("session_id", None)
-    if session:
-        user = get_user_profile(session)
 
-        if user is None:
-            return redirect("https://100014.pythonanywhere.com/")
-        else:
-            print(user)
-            response_data = {'error': True, 'status': "not ok","user":user}
-            return JsonResponse(response_data)
-
-
-    else:
-        return redirect("https://100014.pythonanywhere.com/")
 
 def get_event_id():
     dd = datetime.now()
